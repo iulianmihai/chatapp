54e58be84046cceb31824ad3df624e32657a9c16
Iulian Mihai <imihai@salesforce.com>
2017-02-17 21:57:15 -0500

    Add functionality to serve static files to the web browser
    I added functionality to create a server in app.js and
    to be able to handle http request by serving the files
    and retuning proper error when is the case.
    the server will run on port 3000.

---
+++ app.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+var send404 = function (res) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('Error 404: resource not found.');
+    res.end();
+};
+
+var sendFile = function (res, filePath, fileContents) {
+    res.writeHead(200, {
+        'Content-Type': mime.lookup(path.basename(filePath))
+    });
+    res.end(fileContents);
+};
+
+var serveStatic = function (res, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(res, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+               fs.readFile(absPath, function (err, data) {
+                  if (err) {
+                      send404(res);
+                  }  else {
+                      cache[absPath] = data;
+                      sendFile(res, absPath, data);
+                  }
+               });
+            } else {
+                send404(res);
+            }
+        });
+    }
+};
+
+var server = http.createServer(function (req, res) {
+    var filePath = false;
+    if (req.url === '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + req.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(res, cache, absPath);
+});
+
+server.listen(3000, function () {
+    console.log('Server listening on port 3000');
+});
